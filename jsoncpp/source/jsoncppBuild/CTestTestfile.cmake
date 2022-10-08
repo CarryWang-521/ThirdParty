3d48ca722fb85a0fb9370ae210f956b6c2cd7836
CarryWang <17634092932@163.com>
2022-10-08 21:37:10 +0800

    [TrackNo] NA [Description] 上传三方库源文件及编译好的静态库/动态库

---
+++ jsoncpp/source/jsoncppBuild/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/0. WorkSpace/Third-Party/jsoncpp/source/jsoncpp
+# Build directory: E:/0. WorkSpace/Third-Party/jsoncpp/source/jsoncppBuild
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("include")
---
+++ jsoncpp/source/jsoncppBuild/src/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: E:/0. WorkSpace/Third-Party/jsoncpp/source/jsoncpp/src
+# Build directory: E:/0. WorkSpace/Third-Party/jsoncpp/source/jsoncppBuild/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib_json")
+subdirs("jsontestrunner")
+subdirs("test_lib_json")
